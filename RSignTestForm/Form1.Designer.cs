0d6a84adc591f150bd4f605c4a8ba7d9d2ea0a8e
James O'Reilly <james.oreilly@fernsoftware.com>
2019-08-16 14:43:14 +0100

    Test Project
    Added test project.

---
+++ RSignTestForm/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace RSignTestForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnSend = new System.Windows.Forms.Button();
+            this.BtnGetStatus = new System.Windows.Forms.Button();
+            this.TxtEnvelopeDisplayCode = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.BtnSendPdfs = new System.Windows.Forms.Button();
+            this.TxtEnvelopeId = new System.Windows.Forms.TextBox();
+            this.LblEnvelopeDisplayCode = new System.Windows.Forms.Label();
+            this.LblEnvelopeId = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnSend
+            // 
+            this.BtnSend.Location = new System.Drawing.Point(357, 41);
+            this.BtnSend.Name = "BtnSend";
+            this.BtnSend.Size = new System.Drawing.Size(75, 23);
+            this.BtnSend.TabIndex = 1;
+            this.BtnSend.Text = "Send Bytes";
+            this.BtnSend.UseVisualStyleBackColor = true;
+            this.BtnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // BtnGetStatus
+            // 
+            this.BtnGetStatus.Location = new System.Drawing.Point(357, 70);
+            this.BtnGetStatus.Name = "BtnGetStatus";
+            this.BtnGetStatus.Size = new System.Drawing.Size(75, 23);
+            this.BtnGetStatus.TabIndex = 2;
+            this.BtnGetStatus.Text = "Get Status";
+            this.BtnGetStatus.UseVisualStyleBackColor = true;
+            this.BtnGetStatus.Click += new System.EventHandler(this.BtnGetStatus_Click);
+            // 
+            // TxtEnvelopeDisplayCode
+            // 
+            this.TxtEnvelopeDisplayCode.Location = new System.Drawing.Point(128, 72);
+            this.TxtEnvelopeDisplayCode.Name = "TxtEnvelopeDisplayCode";
+            this.TxtEnvelopeDisplayCode.Size = new System.Drawing.Size(223, 20);
+            this.TxtEnvelopeDisplayCode.TabIndex = 3;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(435, 12);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(323, 173);
+            this.txtStatus.TabIndex = 4;
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Location = new System.Drawing.Point(357, 99);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(75, 23);
+            this.BtnCancel.TabIndex = 5;
+            this.BtnCancel.Text = "Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // BtnSendPdfs
+            // 
+            this.BtnSendPdfs.Location = new System.Drawing.Point(357, 12);
+            this.BtnSendPdfs.Name = "BtnSendPdfs";
+            this.BtnSendPdfs.Size = new System.Drawing.Size(75, 23);
+            this.BtnSendPdfs.TabIndex = 6;
+            this.BtnSendPdfs.Text = "Send Pdf";
+            this.BtnSendPdfs.UseVisualStyleBackColor = true;
+            this.BtnSendPdfs.Click += new System.EventHandler(this.BtnSendPdfs_Click);
+            // 
+            // TxtEnvelopeId
+            // 
+            this.TxtEnvelopeId.Location = new System.Drawing.Point(128, 101);
+            this.TxtEnvelopeId.Name = "TxtEnvelopeId";
+            this.TxtEnvelopeId.Size = new System.Drawing.Size(223, 20);
+            this.TxtEnvelopeId.TabIndex = 7;
+            // 
+            // LblEnvelopeDisplayCode
+            // 
+            this.LblEnvelopeDisplayCode.AutoSize = true;
+            this.LblEnvelopeDisplayCode.Location = new System.Drawing.Point(5, 75);
+            this.LblEnvelopeDisplayCode.Name = "LblEnvelopeDisplayCode";
+            this.LblEnvelopeDisplayCode.Size = new System.Drawing.Size(120, 13);
+            this.LblEnvelopeDisplayCode.TabIndex = 8;
+            this.LblEnvelopeDisplayCode.Text = "Envelope Display Code:";
+            // 
+            // LblEnvelopeId
+            // 
+            this.LblEnvelopeId.AutoSize = true;
+            this.LblEnvelopeId.Location = new System.Drawing.Point(5, 104);
+            this.LblEnvelopeId.Name = "LblEnvelopeId";
+            this.LblEnvelopeId.Size = new System.Drawing.Size(67, 13);
+            this.LblEnvelopeId.TabIndex = 9;
+            this.LblEnvelopeId.Text = "Envelope Id:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 414);
+            this.Controls.Add(this.LblEnvelopeId);
+            this.Controls.Add(this.LblEnvelopeDisplayCode);
+            this.Controls.Add(this.TxtEnvelopeId);
+            this.Controls.Add(this.BtnSendPdfs);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.TxtEnvelopeDisplayCode);
+            this.Controls.Add(this.BtnGetStatus);
+            this.Controls.Add(this.BtnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnSend;
+        private System.Windows.Forms.Button BtnGetStatus;
+        private System.Windows.Forms.TextBox TxtEnvelopeDisplayCode;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.Button BtnSendPdfs;
+        private System.Windows.Forms.TextBox TxtEnvelopeId;
+        private System.Windows.Forms.Label LblEnvelopeDisplayCode;
+        private System.Windows.Forms.Label LblEnvelopeId;
+    }
+}
+
---
+++ RSignTestForm/Form1.cs
@@ -0,0 +1,166 @@
+﻿using CrystalDecisions.Shared;
+using RSignSDK;
+using RSignSDK.Models;
+using RSignSDK.Models.Authentication;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RSignTestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSendPdfs_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TestReport oRpt = new TestReport();
+
+                var credentials = new RSignAPICredentials("aaron.cullen@fernsoftware.com", "Whatever1!", "OQAxADgAQwBFADYAOQBEA");
+                var options = new RSignAPIOptions
+                {
+                    CultureInfo = "en-us",
+                };
+
+                var file = @"C:\Users\James\Desktop\Work\MOZY Datasheet-ReportsAPI.pdf";
+                using (var api = new RSignAPI(credentials, options))
+                {
+                    var envelopeId = api.Send(
+                        file,
+                        "Loan Contract.pdf",
+                        "Template_Test",
+                        "james.oreilly@fernsoftware.com",
+                        "Mr. James",
+                        "Have a loan on us",
+                        "An email body :o",
+                        "60 Days");
+
+                    Console.WriteLine($"The signing request has been sent and responded with the identifier {envelopeId}");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TestReport oRpt = new TestReport();
+
+                var credentials = new RSignAPICredentials("aaron.cullen@fernsoftware.com", "Whatever1!", "OQAxADgAQwBFADYAOQBEA");
+                var options = new RSignAPIOptions
+                {
+                    CultureInfo = "en-us"
+                };
+
+                var expiryType = "60 Days";
+                var docNum = 1;
+                //var emailBody = $@"<html><head><meta http - equiv = ""Content-Type"" content = ""text/html; charset=us-ascii""></head>
+                // var emailBody = $@"<body><div><p style=""font - size:18px;"">This esignature document expires in <b>{expiryType}</b></p></div></body></html>";
+                // var emailBody = $@"<body><div><p style=""font-size:48px"">This esignature document expires in <b>{expiryType}</b></p></div></body>";
+                var emailBody = $@"<body><div><p style=""font-size:32px"">You have <b>{docNum}</b> document(s) to sign.</p>
+                    <p style=""font-size:32px"">This e-signature request expires in <b>{expiryType}</b></p></div></body>";
+
+                //var emailBody = string.Concat("This esignature document expires in ", "60 Days");
+
+                var documentSend = new List<DocumentSend>();
+                using (var s = oRpt.ExportToStream(ExportFormatType.PortableDocFormat))
+                {
+                    using (var reader = new BinaryReader(s))
+                    {
+                        var byteDoc = reader.ReadBytes(Convert.ToInt32(reader.BaseStream.Length));
+                        documentSend.Add(
+                            new DocumentSend
+                            {
+                                DocumentBytes = byteDoc,
+                                DocumentName = "Contacts.pdf"
+                            }
+                       );
+                    }
+                }
+
+                using (var api = new RSignAPI(credentials, options))
+                {
+                    var envelopeId = api.Send(
+                        documentSend,
+                        "Integration_Test",
+                        "james.oreilly@fernsoftware.com",
+                        "Mr. James",
+                        "Have a loan on us",
+                        emailBody,
+                        expiryType);
+
+                    Console.WriteLine($"The signing request has been sent and responded with the identifier {envelopeId}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void BtnGetStatus_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var credentials = new RSignAPICredentials("aaron.cullen@fernsoftware.com", "Whatever1!", "OQAxADgAQwBFADYAOQBEA");
+                var options = new RSignAPIOptions
+                {
+                    CultureInfo = "en-us",
+                };
+
+                using (var api = new RSignAPI(credentials, options))
+                {
+                    var status = api.GetEnvelopeStatus(TxtEnvelopeDisplayCode.Text);
+
+                    txtStatus.Text = status.Message;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var credentials = new RSignAPICredentials("aaron.cullen@fernsoftware.com", "Whatever1!", "OQAxADgAQwBFADYAOQBEA");
+                var options = new RSignAPIOptions
+                {
+                    CultureInfo = "en-us",
+                };
+
+                using (var api = new RSignAPI(credentials, options))
+                {
+                    var statusInfo = api.GetEnvelopeStatus(TxtEnvelopeDisplayCode.Text);
+                    var completed = api.CancelEnvelope(statusInfo.EnvelopeId);
+                    var status = api.GetEnvelopeStatus(TxtEnvelopeDisplayCode.Text);
+
+                    txtStatus.Text = status.Message;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
